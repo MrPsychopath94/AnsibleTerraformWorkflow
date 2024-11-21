def30a383b58f9eff9778085efb1d631e74b7c80
MarcEhrlich <165007370+MarcEhrlich@users.noreply.github.com>
2024-11-21 18:13:16 +0100

    Third Try

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "github-actions-terraformstate-bucket"
-    key    = "github-actions.terraformstate"
-    region = "eu-central-1"
-    encrypt = true 
+    bucket = "github-actions-tfstate-bucket"
+    key    = "github-actions.tfstate"
+    region = "eu-central-1" 
   }
 }
